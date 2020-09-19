e547b51d545aef6224289beb5ace94ae4f9e0c81
baohuynhlam <shinsusenju@aol.com>
2020-09-19 12:12:07 +0700

    Refactor code: Organize into classes and functions

--- scraper.py
+++ scraper.py
@@ -3,125 +3,72 @@ from selenium import webdriver
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
+import constants as const
+from constants import DEADLINE, ADMISSION, COST
 
 URL = "https://bigfuture.collegeboard.org/college-university-search/drexel-university"
 
 
-# Category's link text for Selenium to find
-DEADLINE = "Deadlines"
-ADMISSION = "Applying"
-MAJOR = "Majors & Learning Environment"
-INTERNATIONAL = "International Students"
-COST = "Paying"
+class CollegeBoardScaper:
+    def __init__(self, web_driver):
+        self.driver = web_driver
+    
+    def click(self, element_to_click):
+        """
+        Special click using Javascript because normal Selenium click does not work
+        """
+        self.driver.execute_script("arguments[0].click();", element_to_click)
 
-class InfoElement:
-    def __init__(self, category, description):
-        self.category = category
-        self.xpath = ""
-        self.content = ""
-        self.desp = description
+    def find_and_set_content(self, info_element):
+        info_element.content = self.driver.find_element_by_xpath(info_element.xpath).text
+        print(f"{info_element.desp}: {info_element.content}")
 
-    def find_and_set_content(self):
-        global driver
-        self.content = driver.find_element_by_xpath(self.xpath).text
-        print(f"{self.desp}: {self.content}")
+    def scrape_deadlines(self):
+        deadlines_tab = self.driver.find_element_by_link_text(DEADLINE)
+        self.click(deadlines_tab)
+        # For loops to scrape all elements at once
+        for (date_element, month_element) in const.DEADLINE_ELEMENTS:
+            self.find_and_set_content(date_element)
+            self.find_and_set_content(month_element)
+    
+    def scrape_admission(self):
+        # First click on admission tab for website to dynamically generate admission data
+        admission_tab = self.driver.find_element_by_link_text(ADMISSION)
+        self.click(admission_tab)
+        # Get addmission rate
+        self.find_and_set_content(const.admit_rate)
+        # Additional click to open "SAT and ACT" tab
+        sat_act_tab = driver.find_element_by_link_text("SAT & ACT Scores")
+        self.click(sat_act_tab)
+        # Additional clicks to access specific "SAT" or "ACT" tab
+        sat_inner_tab = driver.find_element_by_xpath('/html/body/div[9]/div/div/div[3]/div[2]/div[2]/div/div/div[3]/div/div/div[2]/div/div[1]/button')
+        self.click(sat_inner_tab)
+        self.find_and_set_content(const.sat_range)
 
-# Deadlines info
-class Deadline:
-    def __init__(self, description):    
-        self.date = InfoElement(DEADLINE, description + " Date")
-        self.month = InfoElement(DEADLINE, description + " Month")
+        act_inner_tab = driver.find_element_by_xpath('/html/body/div[9]/div/div/div[3]/div[2]/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/button')
+        self.click(act_inner_tab)
+        self.find_and_set_content(const.act_range)
 
-rd = Deadline("Regular decision")
-rd.month.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]'
-rd.date.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[1]/div[3]/div[2]'
+    def scrape_cost(self):
+        cost_tab = self.driver.find_element_by_link_text(COST)
+        self.click(cost_tab)
 
+        out_of_state_tab = self.driver.find_element_by_xpath('//*[@id="cpProfile_tabs_paying_outstatecosts_anchor"]/a')
+        self.click(out_of_state_tab)
 
-ea = Deadline("Early action")
-ea.month.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]'
-ea.date.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]'
+        for element in const.COST_ELEMENTS:
+            self.find_and_set_content(element)
 
-ed1 = Deadline("Early decision 1")
-ed1.month.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[1]/div[5]/div[1]'
-ed1.date.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[1]/div[5]/div[2]'
+    def scrape_all(self):
+        self.scrape_admission()
+        self.scrape_deadlines()
+        self.scrape_cost()
+        self.driver.quit()
+    
 
-ed2 = Deadline("Early decision 2")
-ed2.month.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]'
-ed2.date.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]'
-
-# Generating deadline elements list
-deadline_object_list = [rd, ea, ed1, ed2]
-DEADLINE_ELEMENTS = []
-for deadline_object in deadline_object_list:
-    DEADLINE_ELEMENTS.append(deadline_object.date)
-    DEADLINE_ELEMENTS.append(deadline_object.month)
-
-# Admission info: admission rate, SAT, etc.
-
-admit_rate = InfoElement(ADMISSION, "Admission rate")
-admit_rate.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[1]/div[1]/div[3]/p/div[3]'
-
-# Must click additionally on the "SAT & ACT Scores" tab first (after clicking the "Applying" tab)
-# to access SAT/ACT info
-sat = InfoElement(ADMISSION, "SAT range")
-sat.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div[1]/div/div/div[1]/table/tbody/tr/td[2]/div/div[1]/div[3]/div[2]/div[2]/div[1]/div'
-
-act = InfoElement(ADMISSION, "ACT range")
-act.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div[2]/div/div/div[1]/table/tbody/tr/td[2]/div/div[1]/div[2]/div[2]/div[1]/div'
-
-ADMISSION_ELEMENTS = [sat, act, admit_rate]
-
-# Cost info
-# Must click on "Out-Of-State Costs" tab first
-out_tuition = InfoElement(COST, "Out-of-state tuition")
-out_tuition.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[4]/div[1]/div/div/div/table/tbody/tr[1]/td[1]/span'
-
-room = InfoElement(COST, "Room and board")
-room.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[4]/div[1]/div/div/div/table/tbody/tr[2]/td[1]/span'
-
-out_total = InfoElement(COST, "Out-of-state total cost")
-out_total.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[4]/div[1]/div/div/div/table/tbody/tr[7]/td[1]/span'
-
-COST_ELEMENTS = [out_tuition, room, out_total]
-
-def click(element_to_click):
-    global driver
-    driver.execute_script("arguments[0].click();", element_to_click)
-
-
-# Test 1
-driver = webdriver.Safari()
-driver.get(URL)
-driver.implicitly_wait(15)
-
-# Getting deadlines info
-deadlines_tab = driver.find_element_by_link_text(DEADLINE)
-click(deadlines_tab)
-for element in DEADLINE_ELEMENTS:
-    element.find_and_set_content()
-
-# Getting admission info
-admission_tab = driver.find_element_by_link_text(ADMISSION)
-click(admission_tab)
-admit_rate.find_and_set_content()
-# Additional click to open "SAT and ACT" tab
-sat_act_tab = driver.find_element_by_link_text("SAT & ACT Scores")
-click(sat_act_tab)
-
-sat_inner_tab = driver.find_element_by_xpath('/html/body/div[9]/div/div/div[3]/div[2]/div[2]/div/div/div[3]/div/div/div[2]/div/div[1]/button')
-click(sat_inner_tab)
-sat.find_and_set_content()
-
-act_inner_tab = driver.find_element_by_xpath('/html/body/div[9]/div/div/div[3]/div[2]/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/button')
-click(act_inner_tab)
-act.find_and_set_content()
-
-# Getting cost info
-cost_tab = driver.find_element_by_link_text(COST)
-click(cost_tab)
-click(driver.find_element_by_link_text("Out-Of-State Costs"))
-
-for element in COST_ELEMENTS:
-    element.find_and_set_content()
-
-driver.quit()
+if __name__ == "__main__":
+    driver = webdriver.Safari()
+    driver.get(URL)
+    driver.implicitly_wait(15)
+    scraper_session = CollegeBoardScaper(driver)
+    scraper_session.scrape_all()
