be2694bdf9bb1323dc9ad5780b368b5da1d1e4d3
baohuynhlam <shinsusenju@aol.com>
2020-09-22 09:59:14 +0700

    Refactor code: Separate immutable constants and result data better

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import data
+import values
 from constants import DEADLINE, ADMISSION, COST, BIGFUTURE_DOMAIN, BIGFUTURE_ERROR
 import excel
 
@@ -11,12 +11,25 @@ def generate_URL(school_name):
     school_url_path = school_name.replace(" ", "-").lower()
     return BIGFUTURE_DOMAIN + school_url_path
 
+def construct_school_dict(school_name):
+    """
+    A dictionary template to generate a dictionary
+    to store scraped information about each school
+    """
+    school = dict()
+    school["Name"] = school_name
+    for field in values.FIELD_NAMES:
+        school[field] = ""
+    return school
+
+
 class CollegeBoardScaper:
     """
     Initiating a scraper session
     """
-    def __init__(self, web_driver):
+    def __init__(self, web_driver, school_name):
         self.driver = web_driver
+        self.school_info = construct_school_dict(school_name)
     
     def click(self, element_to_click):
         """
@@ -24,19 +37,20 @@ class CollegeBoardScaper:
         """
         self.driver.execute_script("arguments[0].click();", element_to_click)
 
-    def check_valid_page(self, school):
+    def check_valid_page(self):
         if (self.driver.current_url == BIGFUTURE_ERROR):
-            raise NotFoundError(school)
+            raise NotFoundError()
 
     def find_and_set_content(self, info_element):
-        info_element.content = self.driver.find_element_by_xpath(info_element.xpath).text
-        # print(f"{info_element.desp}: {info_element.content}")
+        content = self.driver.find_element_by_xpath(info_element.xpath).text
+        self.school_info[info_element.desp] = content
+    #     # print(f"{info_element.desp}: {info_element.content}")
 
     def scrape_deadlines(self):
         deadlines_tab = self.driver.find_element_by_link_text(DEADLINE)
         self.click(deadlines_tab)
         # For loops to scrape all elements at once
-        for (date_element, month_element) in data.DEADLINE_TUPLE_ELEMENTS:
+        for (date_element, month_element) in values.DEADLINE_TUPLE_ELEMENTS:
             self.find_and_set_content(date_element)
             self.find_and_set_content(month_element)
     
@@ -45,18 +59,18 @@ class CollegeBoardScaper:
         admission_tab = self.driver.find_element_by_link_text(ADMISSION)
         self.click(admission_tab)
         # Get addmission rate
-        self.find_and_set_content(data.admit_rate)
+        self.find_and_set_content(values.ADMIT_RATE)
         # Additional click to open "SAT and ACT" tab
         sat_act_tab = self.driver.find_element_by_link_text("SAT & ACT Scores")
         self.click(sat_act_tab)
         # Additional clicks to access specific "SAT" or "ACT" tab
         sat_inner_tab = self.driver.find_element_by_xpath('/html/body/div[9]/div/div/div[3]/div[2]/div[2]/div/div/div[3]/div/div/div[2]/div/div[1]/button')
         self.click(sat_inner_tab)
-        self.find_and_set_content(data.sat_range)
+        self.find_and_set_content(values.SAT_RANGE)
 
         act_inner_tab = self.driver.find_element_by_xpath('/html/body/div[9]/div/div/div[3]/div[2]/div[2]/div/div/div[3]/div/div/div[2]/div/div[2]/button')
         self.click(act_inner_tab)
-        self.find_and_set_content(data.act_range)
+        self.find_and_set_content(values.ACT_RANGE)
 
     def scrape_cost(self):
         cost_tab = self.driver.find_element_by_link_text(COST)
@@ -69,7 +83,7 @@ class CollegeBoardScaper:
         
         self.click(out_of_state_tab)
 
-        for element in data.COST_ELEMENTS:
+        for element in values.COST_ELEMENTS:
             self.find_and_set_content(element)
 
     def scrape_all(self):
@@ -79,8 +93,7 @@ class CollegeBoardScaper:
  
 class NotFoundError(Exception):
     """Exception raised for school names that cannot be found on CollegeBoard"""
-    def __init__(self, invalid_school, message="Cannot find school"):
-        self.school = invalid_school
+    def __init__(self, message="Cannot find school"):
         self.message = message
         super().__init__(self.message)
 
@@ -90,10 +103,10 @@ def run_from_command_line():
     URL = generate_URL(school)
     driver = webdriver.Safari()
     driver.implicitly_wait(15)
-    scraper_session = CollegeBoardScaper(driver)
+    scraper_session = CollegeBoardScaper(driver, school)
     try:
         driver.get(URL)
-        scraper_session.check_valid_page(school)
+        scraper_session.check_valid_page()
         scraper_session.scrape_all()
     except NotFoundError:
         print(f"'{school}' cannot be found. Make sure to type in the school's full name correctly")
@@ -104,26 +117,24 @@ def run_from_command_line():
 
 
 def run_from_file(input_file) -> dict:
-    all_results = [] # List of dictionaries to store all resulets
+    all_results = [] # List of dictionaries to store all results
     driver = webdriver.Safari()
     driver.implicitly_wait(10)
-    scraper_session = CollegeBoardScaper(driver)
     with open(input_file) as school_list:
-        for school in school_list:
-            school = school.rstrip()
-            print(school)
-            URL = generate_URL(school)
+        for school_name in school_list:
+            school_name = school_name.rstrip()
+            print(school_name)
+            URL = generate_URL(school_name)
+            scraper_session = CollegeBoardScaper(driver, school_name)
             try:
                 driver.get(URL)
-                scraper_session.check_valid_page(school)
+                scraper_session.check_valid_page()
                 scraper_session.scrape_all()
-
-                school_result = data.generate_dict(school_name=school)
-                all_results.append(school_result)
+                all_results.append(scraper_session.school_info)
             except NotFoundError:
-                print(f"'{school}' cannot be found. Make sure to type in the school's full name correctly")
+                print(f"'{school_name}' cannot be found. Make sure to type in the school's full name correctly")
             except TimeoutException:
-                print(f"Timeout for {school}. Check network connection")
+                print(f"Timeout for {school_name}. Check network connection")
             finally:
                 print("Done")
     driver.quit()
---
+++ values.py
@@ -0,0 +1,79 @@
+"""
+File to store information and xpath data
+"""
+
+from constants import DEADLINE, COST, ADMISSION
+
+
+class InfoElement:
+    """
+    A helper class to store and organize information
+    """
+
+    def __init__(self, category, description):
+        self.category = category
+        self.xpath = ""
+        self.desp = description
+
+
+RD_MONTH = InfoElement(DEADLINE, "RD Month")
+RD_MONTH.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]'
+
+RD_DATE = InfoElement(DEADLINE, "RD DATE")
+RD_DATE.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[1]/div[3]/div[2]'
+
+EA_MONTH = InfoElement(DEADLINE, "EA Month")
+EA_MONTH.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]'
+
+EA_DATE = InfoElement(DEADLINE, "EA Date")
+EA_DATE.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]'
+
+ED1_MONTH = InfoElement(DEADLINE, "ED1 Month")
+ED1_MONTH.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[1]/div[5]/div[1]'
+
+ED1_DATE = InfoElement(DEADLINE, "ED1 Date")
+ED1_DATE.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[1]/div[5]/div[2]'
+
+ED2_MONTH = InfoElement(DEADLINE, "ED2 Month")
+ED2_MONTH.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]'
+
+ED2_DATE = InfoElement(DEADLINE, "ED2 Date")
+ED2_DATE.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]'
+
+DEADLINE_TUPLE_ELEMENTS = [(RD_DATE, RD_MONTH), (EA_DATE, EA_MONTH),
+                           (ED1_DATE, ED1_MONTH), (ED2_DATE, ED2_MONTH)]
+DEADLINE_ELEMENTS = [x for datemonth_tuple in DEADLINE_TUPLE_ELEMENTS for x in datemonth_tuple]
+
+
+# Admission info: admission rate, SAT, etc.
+ADMIT_RATE = InfoElement(ADMISSION, "Admission rate")
+ADMIT_RATE.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[1]/div[1]/div[3]/p/div[3]'
+
+# Must click additionally on the "SAT & ACT Scores" tab first (after clicking the "Applying" tab)
+# to access SAT/ACT info
+SAT_RANGE = InfoElement(ADMISSION, "SAT range")
+SAT_RANGE.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div[1]/div/div/div[1]/table/tbody/tr/td[2]/div/div[1]/div[3]/div[2]/div[2]/div[1]/div'
+
+ACT_RANGE = InfoElement(ADMISSION, "ACT range")
+ACT_RANGE.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[3]/div[2]/div/div/div[1]/table/tbody/tr/td[2]/div/div[1]/div[2]/div[2]/div[1]/div'
+
+ADMISSION_ELEMENTS = [ADMIT_RATE, SAT_RANGE, ACT_RANGE]
+
+
+# Cost info
+# Must click on "Out-Of-State Costs" tab first
+OUT_TUITION = InfoElement(COST, "Out-of-state tuition")
+OUT_TUITION.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[4]/div[1]/div/div/div/table/tbody/tr[1]/td[1]/span'
+
+ROOM = InfoElement(COST, "ROOM and boaRD")
+ROOM.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[4]/div[1]/div/div/div/table/tbody/tr[2]/td[1]/span'
+
+OUT_TOTAL = InfoElement(COST, "Out-of-state total cost")
+OUT_TOTAL.xpath = '//*[@id="topFrame"]/div[2]/div[2]/div/div/div[3]/div/div/div[4]/div[1]/div/div/div/table/tbody/tr[7]/td[1]/span'
+
+
+COST_ELEMENTS = [OUT_TUITION, ROOM, OUT_TOTAL]
+
+ALL_ELEMENTS = ADMISSION_ELEMENTS + DEADLINE_ELEMENTS + COST_ELEMENTS
+
+FIELD_NAMES = [x.desp for x in ALL_ELEMENTS]
